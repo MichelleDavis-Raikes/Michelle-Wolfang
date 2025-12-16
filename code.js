5e20712676faa610c7c2423c85f80a95ee9140aa
MichelleDavis-Raikes <mdavisraikes92105@uat.edu>
2025-12-15 22:31:43 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,52 @@
+
+// functions are a nice way to organize our code and make our code reusable
+// use a descriptive function name - functions do something, so make it a verb/action
+// () can hold function argument - if you need to pass information to the function
+function playDice(){
+    // log that the function was called
+    console.log("playDice() function was called");
+
+    // roll die 1
+    let roll1 = rollDice();
+
+    // roll die 2
+    let roll2 = rollDice();
+
+    console.log("roll1=" + roll1)
+    console.log("roll2=" + roll2)
+
+    // calculate the rolls
+    let sum = roll1 + roll2;
+
+    // see if the user lost - if they rolled a sum of 7 or 11
+    // || - means or
+    // && - means and
+    if(sum == 7 || sum == 11){
+        console.log("you lost")
+    }
+    // checkl if we rolled for an even number
+    // modulus - do a division problem and we get a remainder as the results
+    // 2/2 = 1 with no remainder = even numbers;
+    // 4/2 = 2 with no remainder = even numbers;
+    // 5/2 = 2 with a remainder of 1 = odd number
+    // roll1 % 2 == 0 - this checks will be true if we have an even number
+    else if(roll1 % 2 == 0){
+    
+    }
+
+    document.getElementById("divSum").textContent = sum;
+}
+
+// craete a functions to generate a random number
+function rollDice(){
+    // create a varibale to hold a random number
+    // Math.random() is a built in JavaScript function which generates a random number
+    let die = Math.random() * 6; // I want to replicate a 6 sided die
+
+    // round the number and return the value
+    return Math.ceil(die);
+}
+
 function testPalin()
 {
     // get the value that the user entered in the textbox
